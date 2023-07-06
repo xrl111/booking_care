b12e95b73984fc6653bb4e6e2bdd27bf7c1c2ced
Ender <buiduclan000@gmail.com>
2023-07-06 12:01:28 +0700

    fix create new user service

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -10,7 +10,7 @@ const sequelize = new Sequelize('booking_care', 'root', null, {
 let connectDB = async () => {
     try {
         await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
+        console.log('DB Connection has been established successfully.');
     } catch (error) {
         console.error('Unable to connect to the database:', error);
     }
--- src/services/userService.js
+++ src/services/userService.js
@@ -102,25 +102,27 @@ let createNewUser = (data) => {
             if (check === true) {
                 resolve({
                     errCode: 1,
-                    message: 'your email is already in used'
+                    errMessage: 'your email is already in used'
                 })
+            }else{
+                let hashPasswordFromBcrypt = await hashUserPassword(data.password)
+                await db.User.create({
+                    email: data.email,
+                    password: hashPasswordFromBcrypt,
+                    firstName: data.firstName,
+                    lastName: data.lastName,
+                    address: data.address,
+                    phoneNumber: data.phoneNumber,
+                    gender: data.gender === '1' ? true : false,
+                    roleId: data.roleId
+                })
+                resolve({
+                    errCode: 0,
+                    message: 'OK'
+                });
             }
 
-            let hashPasswordFromBcrypt = await hashUserPassword(data.password)
-            await db.User.create({
-                email: data.email,
-                password: hashPasswordFromBcrypt,
-                firstName: data.firstName,
-                lastName: data.lastName,
-                address: data.address,
-                phoneNumber: data.phoneNumber,
-                gender: data.gender === '1' ? true : false,
-                roleId: data.roleId
-            })
-            resolve({
-                errCode: 0,
-                message: 'OK'
-            });
+          
         } catch (e) {
             reject(e);
         }
