bf003b7c30001b4cb7d6447a9d6c109344b0d3cd
xrl111 <93368873+xrl111@users.noreply.github.com>
2023-07-27 10:41:29 +0700

    manage create user redux

--- src/services/userService.js
+++ src/services/userService.js
@@ -22,7 +22,7 @@ let handleUserLogin = (email, password) => {
 
             if (isExist) {
                 let user = await db.User.findOne({
-                    attributes: ['email', 'roleId', 'password'],
+                    attributes: ['email', 'roleId', 'password', 'firstName', 'lastName'],
                     where: { email: email },
                     raw: true
                 })
@@ -113,8 +113,9 @@ let createNewUser = (data) => {
                     lastName: data.lastName,
                     address: data.address,
                     phoneNumber: data.phoneNumber,
-                    gender: data.gender === '1' ? true : false,
-                    roleId: data.roleId
+                    gender: data.gender,
+                    roleId: data.roleId,
+                    positionId: data.positionId,
                 })
                 resolve({
                     errCode: 0,
